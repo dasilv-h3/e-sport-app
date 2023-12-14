e7a55dbfc63bdd53d35d303d1547575b4e446efb
H3Dylan <d.bouillon@h3hitema.fr>
2023-12-14 15:51:03 +0100

    ordre

--- esportApp/src/Components/Description/Cs.jsx
+++ esportApp/src/Components/Description/Cs.jsx
@@ -7,6 +7,7 @@ const Cs = () => {
             <div className="desc">
                 <h1>Description</h1>
                 <p>Counter-Strike: Global Offensive (CS: GO) est un jeu de tir à la première personne compétitif. Les joueurs s'affrontent en équipes, l'une incarnant des terroristes et l'autre des contre-terroristes. L'objectif varie selon le mode de jeu, allant de la pose ou désamorçage de bombes à la libération ou la défense d'otages. CS: GO est connu pour son gameplay tactique, ses armes variées, et son exigence de précision. Les parties sont rapides, intenses et requièrent une coordination d'équipe stratégique pour atteindre la victoire.</p>
+                
             </div>
         </>
     )
